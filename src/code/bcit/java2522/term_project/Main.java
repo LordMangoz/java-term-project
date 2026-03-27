842ff82786b2776307f20f47e8ff6fc50cfd5c20
umanga <umangabajgai@gmail.com>
2026-03-27 00:42:25 -0700

    inital commit

---
+++ src/code/bcit/java2522/term_project/Main.java
@@ -0,0 +1,8 @@
+package bcit.java2522.term_project;
+
+public class Main {
+    public static void main(final String[] args)
+    {
+
+    }
+}
---
+++ src/code/bcit/java2522/term_project/WordGame/World.java
@@ -0,0 +1,4 @@
+package bcit.java2522.term_project.WordGame;
+
+public class World {
+}
