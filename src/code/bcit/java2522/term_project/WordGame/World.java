e21a756c9d11049aaa792bdf6a3bfcea226aab8f
umanga <umangabajgai@gmail.com>
2026-03-27 07:00:03 -0700

    start wordgame task

--- src/code/bcit/java2522/term_project/WordGame/World.java
+++ src/code/bcit/java2522/term_project/WordGame/World.java
@@ -2,16 +2,49 @@ package bcit.java2522.term_project.WordGame;
 
 import java.util.HashMap;
 import java.util.Map;
-
+import java.util.Scanner;
+import java.io.File;
 /**
  * Represents all the country data.
  * @author Umanga Bajgai
  */
 public class World {
-    final static Map countries;
 
     public World()
     {
+        createCountries();
+    }
+
+    /**
+     * Creates country objects, based on data in resource files.
+     *
+     */
+    private void createCountries()
+    {
+        final String resourceDirectory;
+        final String fileEnding;
+        String relativePath;
+        String currentFile;
+
+        char currentFileChar;
+        File countryFile;
+
+        resourceDirectory = "src/resources/";
+        fileEnding = ".txt";
+        currentFileChar = 'a';
+
+        for (int i = 'a'; i < 'z'; i++)
+        {
+            relativePath = resourceDirectory + currentFileChar + fileEnding;
+            countryFile = new File(relativePath);
+
+            //for-each of the files, we need to create a country object for it.
+
+
+            currentFileChar+=1;
+        }
+
+
 
     }
 
